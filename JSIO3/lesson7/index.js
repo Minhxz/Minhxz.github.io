315360456c3800b8df791da5ea0da6ac5ec7fe3c
Minhxz <87261483+Minhxz@users.noreply.github.com>
2021-12-26 14:27:24 +0700

    yea hw.

---
+++ JSIO3/lesson7/index.js
@@ -0,0 +1,17 @@
+
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+ import { getFirestore,doc,getDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyBbWX5R5SbV3Wpiy_W02Giwm6RQe2btlLM",
+   authDomain: "jsi03-4ae63.firebaseapp.com",
+   projectId: "jsi03-4ae63",
+   storageBucket: "jsi03-4ae63.appspot.com",
+   messagingSenderId: "1000693069688",
+   appId: "1:1000693069688:web:d2af3ce446b1de18fb3a8d"
+ };
